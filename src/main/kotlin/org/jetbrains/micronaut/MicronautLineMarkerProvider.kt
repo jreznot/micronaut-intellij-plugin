ca615ef99a4b99a407205508d3750ddea5e234c0
Yuriy Artamonov <jreznot@yandex.ru>
2019-10-06 21:43:16 +0300

    Publish/Subscribe event navigation #6

--- src/main/kotlin/org/jetbrains/micronaut/MicronautLineMarkerProvider.kt
+++ src/main/kotlin/org/jetbrains/micronaut/MicronautLineMarkerProvider.kt
@@ -4,10 +4,14 @@ import com.intellij.codeInsight.AnnotationUtil
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
+import com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleUtilCore
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.util.NotNullLazyValue
 import com.intellij.psi.*
 import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.GlobalSearchScope.moduleWithDependenciesAndLibrariesScope
 import com.intellij.psi.search.searches.AnnotatedElementsSearch
 import com.intellij.psi.search.searches.MethodReferencesSearch
 import com.intellij.psi.util.CachedValueProvider
@@ -17,8 +21,8 @@ import com.intellij.psi.util.PsiModificationTracker
 import com.intellij.util.SmartList
 import org.jetbrains.micronaut.events.EventSubscription
 import org.jetbrains.micronaut.events.PublishEventPoint
-import org.jetbrains.uast.UCallExpression
-import org.jetbrains.uast.getUastParentOfType
+import org.jetbrains.uast.*
+import java.util.Collections.emptyList
 
 // Example #5: Custom navigation for @EventListener
 class MicronautLineMarkerProvider : RelatedItemLineMarkerProvider() {
@@ -27,15 +31,41 @@ class MicronautLineMarkerProvider : RelatedItemLineMarkerProvider() {
         element: PsiElement,
         result: MutableCollection<in RelatedItemLineMarkerInfo<PsiElement>>
     ) {
-        if (element is PsiIdentifier
-            && element.parent is PsiMethod) {
-            val method = element.parent as PsiMethod
-            if (AnnotationUtil.isAnnotated(method, EVENT_LISTENER, 0)) {
-                val builder = NavigationGutterIconBuilder.create(MicronautIcons.RECEIVER)
-                    .setTooltipText("Navigate to event publisher")
-                    .setTargets(getPublisherTargets(element))
+        val uParent = getUParentForIdentifier(element)
 
-                result.add(builder.createLineMarkerInfo(element))
+        if (uParent is UMethod
+            && isEventListenerMethod(uParent.javaPsi)
+        ) {
+            // Add Line Marker for @EventListener methods
+
+            val builder = NavigationGutterIconBuilder.create(MicronautIcons.RECEIVER)
+                .setAlignment(Alignment.LEFT)
+                .setTooltipText("Navigate to event publisher")
+                .setTargets(getPublisherTargets(element))
+
+            result.add(builder.createLineMarkerInfo(element))
+
+        } else {
+            val uCallExpression = element.toUElementOfType<UCallExpression>()
+            if (uCallExpression != null
+                && uCallExpression.kind == UastCallKind.METHOD_CALL
+                && isPublishEventExpression(uCallExpression)
+            ) {
+                // Add Line Marker for publishEvent(event) expressions
+
+                val sourcePsi = uCallExpression.sourcePsi
+                val identifier = uCallExpression.methodIdentifier.sourcePsiElement
+                if (identifier != null
+                    && sourcePsi != null
+                ) {
+                    val builder = NavigationGutterIconBuilder.create(MicronautIcons.SENDER)
+                        .setAlignment(Alignment.LEFT)
+                        // Example of Lazy marker evaluation
+                        .setTargets(NotNullLazyValue.createValue { findEventListeners(sourcePsi) })
+                        .setTooltipText("Navigate to event listeners")
+
+                    result.add(builder.createLineMarkerInfo(identifier))
+                }
             }
         }
     }
@@ -54,15 +84,33 @@ class MicronautLineMarkerProvider : RelatedItemLineMarkerProvider() {
             .map { it.element as PsiMethodCallExpression }
     }
 
+    private fun findEventListeners(psiElement: PsiElement): Collection<PsiElement> {
+        val module = ModuleUtilCore.findModuleForPsiElement(psiElement) ?: return emptyList()
+        val uCallExpression = psiElement.toUElementOfType<UCallExpression>() ?: return emptyList()
+
+        if (uCallExpression.valueArgumentCount != 1) return emptyList()
+        val eventType = uCallExpression.valueArguments[0].getExpressionType() ?: return emptyList()
+
+        val eventListeners = getEventListeners(module, eventType)
+        val list = ArrayList<PsiElement>(eventListeners.size)
+        for (listener in eventListeners) {
+            val m = listener.method
+            if (m != null) {
+                list.add(m.navigationElement)
+            }
+        }
+        return list
+    }
+
     private fun getPublishPoints(module: Module, eventType: PsiType): Collection<PublishEventPoint> {
-        return getPublishPoints(module).filter { p ->
+        return getPublishPointsCached(module).filter { p ->
             val pointType = p.eventType
             pointType != null && eventType.isAssignableFrom(pointType)
         }
     }
 
     private fun getEventListeners(module: Module, eventType: PsiType): Collection<EventSubscription> {
-        return getEventListeners(module).filter { l ->
+        return getEventListenersCached(module).filter { l ->
             val handlerType = l.eventType
             handlerType != null && eventType.isAssignableFrom(handlerType)
         }
@@ -81,65 +129,74 @@ class MicronautLineMarkerProvider : RelatedItemLineMarkerProvider() {
     }
 
     private fun isEventListenerMethod(psiMethod: PsiMethod): Boolean {
-        if (psiMethod.parameterList.parametersCount == 1
-            && psiMethod.parameterList.parameters[0].type is PsiClassType) {
-
-            if (AnnotationUtil.isAnnotated(psiMethod, EVENT_LISTENER,0)) {
-                return true
-            }
-        }
-
-        return false
+        return psiMethod.parameterList.parametersCount == 1
+                && psiMethod.parameterList.parameters[0].type is PsiClassType
+                && AnnotationUtil.isAnnotated(psiMethod, EVENT_LISTENER, 0)
     }
 
-    private fun getPublishPoints(module: Module): Collection<PublishEventPoint> {
+    private fun getPublishPointsCached(module: Module): Collection<PublishEventPoint> {
+        // Example of CachedValuesManager.getCachedValue()
         val cacheManager = CachedValuesManager.getManager(module.project)
 
         return cacheManager.getCachedValue(module) {
-            val scope = GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module)
+            val scope = moduleWithDependenciesAndLibrariesScope(module)
 
-            val points = SmartList<PublishEventPoint>()
-            for (publishMethod in getPublishMethods(module, scope)) {
-                val entries = MethodReferencesSearch.search(publishMethod, scope, true)
-                for (reference in entries) {
-                    val expression = reference.element.getUastParentOfType<UCallExpression>()
-                    if (expression != null
-                        && expression.valueArgumentCount == 1
-                        && isPublishEventExpression(expression)) {
-                        val arg = expression.valueArguments[0]
-                        points.add(PublishEventPoint(arg))
-                    }
-                }
-            }
-
-            CachedValueProvider.Result.create(points, PsiModificationTracker.MODIFICATION_COUNT)
+            CachedValueProvider.Result.create(
+                getPublishPoints(scope),
+                PsiModificationTracker.MODIFICATION_COUNT,
+                ProjectRootManager.getInstance(module.project)
+            )
         }
     }
 
-    private fun getPublishMethods(module: Module, scope: GlobalSearchScope): Array<PsiMethod> {
-        val javaPsiFacade = JavaPsiFacade.getInstance(module.project)
+    // MethodReferencesSearch Usage Example
+    private fun getPublishPoints(scope: GlobalSearchScope): SmartList<PublishEventPoint> {
+        val points = SmartList<PublishEventPoint>()
+        for (publishMethod in getPublishMethods(scope)) {
+            val entries = MethodReferencesSearch.search(publishMethod, scope, true)
+            for (reference in entries) {
+                val expression = reference.element.getUastParentOfType<UCallExpression>()
+                if (expression != null
+                    && expression.valueArgumentCount == 1
+                ) {
+                    points.add(PublishEventPoint(expression.valueArguments[0]))
+                }
+            }
+        }
+        return points
+    }
+
+    private fun getPublishMethods(scope: GlobalSearchScope): Array<PsiMethod> {
+        val javaPsiFacade = JavaPsiFacade.getInstance(scope.project)
         val publisherClass = javaPsiFacade.findClass(EVENT_PUBLISHER, scope) ?: return emptyArray()
 
         return publisherClass.findMethodsByName(PUBLISH_EVENT_METHOD, false)
     }
 
-    private fun getEventListeners(module: Module): Collection<EventSubscription> {
+    private fun getEventListenersCached(module: Module): Collection<EventSubscription> {
         val cacheManager = CachedValuesManager.getManager(module.project)
 
         return cacheManager.getCachedValue(module) {
-            val scope = GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module)
+            val scope = moduleWithDependenciesAndLibrariesScope(module)
 
-            val javaPsiFacade = JavaPsiFacade.getInstance(module.project)
-            val eventListenerClass = javaPsiFacade.findClass(EVENT_LISTENER, scope)
-            val collector = SmartList<EventSubscription>()
-            if (eventListenerClass != null) {
-                val listenerMethods = AnnotatedElementsSearch.searchPsiMethods(eventListenerClass, scope)
-                for (element in listenerMethods) {
-                    collector.add(EventSubscription(element))
-                }
-            }
-
-            CachedValueProvider.Result.create(collector, PsiModificationTracker.MODIFICATION_COUNT)
+            CachedValueProvider.Result.create(
+                getEventListeners(scope),
+                PsiModificationTracker.MODIFICATION_COUNT,
+                ProjectRootManager.getInstance(module.project)
+            )
         }
+    }
+
+    private fun getEventListeners(scope: GlobalSearchScope): SmartList<EventSubscription> {
+        val collector = SmartList<EventSubscription>()
+        val javaPsiFacade = JavaPsiFacade.getInstance(scope.project)
+        val eventListenerClass = javaPsiFacade.findClass(EVENT_LISTENER, scope)
+        if (eventListenerClass != null) {
+            val listenerMethods = AnnotatedElementsSearch.searchPsiMethods(eventListenerClass, scope)
+            for (element in listenerMethods) {
+                collector.add(EventSubscription(element))
+            }
+        }
+        return collector
     }
 }
